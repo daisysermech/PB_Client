0f79dde2e2a8a785926222bd88fa35567d2aa16d
Вероника Магдысюк <Veronika.khust.1998@gmail.com>
2023-05-29 16:02:07 +0300

    upd

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Net.Client;
+using SumClientApp;
+ 
+// создаем канал для обмена сообщениями с сервером
+// параметр - адрес сервера gRPC
+using var channel = GrpcChannel.ForAddress("http://localhost:5102");
+// создаем клиент
+var client = new SumClientApp.SumService.SumServiceClient(channel);
+
+var nn = new NumbersRequest();
+nn.Number1=Int32.Parse(Console.ReadLine());
+nn.Number2=Int32.Parse(Console.ReadLine());
+
+var reply = client.Summing(nn);
+Console.WriteLine($"Відповідь: {reply.Sum}");
+
